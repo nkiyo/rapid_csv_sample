eff6883c376e93317daa0835f269ceeb256c3138
nkiyo <nakahara.kiyoshi@gmail.com>
2023-08-27 19:58:07 +0900

    up

--- main.cpp
+++ main.cpp
@@ -7,9 +7,10 @@ using std::string;
 using std::vector;
 
 int main() {
+  // 1列目(ID)を row header として、csvを読む
   rapidcsv::Document doc("csv/sample.csv", rapidcsv::LabelParams(0, 0));
 
-  // 列単位で取り出す(int) => row header となる列はGetColumnでは取得できない
+  // 列単位で取り出す(int) => row header となる列はGetColumnでは取得できないので注意
   //vector<int> col1 = doc.GetColumn<int>("ID");
   //for (auto cell: col1)
   //  printf("%d ", cell);
@@ -32,17 +33,32 @@ int main() {
   double d = doc.GetCell<double>("Float", "10");
   printf("%f\n", d);
 
-  // 無効なカラム名を指定した場合
-  //double cell2 = doc.GetCell<double>("NonExistentColumn", "10");
-  // => std::out_of_range
+  try {
+    // 無効なカラム名を指定した場合
+    //double cell2 = doc.GetCell<double>("NonExistentColumn", "10");
+    // => std::out_of_range
 
-  // 無効なrow headerを指定した場合
-  //double cell3 = doc.GetCell<double>("Float", "12345");
-  // => std::out_of_range
-  
-  // TODO 期待するデータ型を取得した場合 
-  // 可能な限り、キャストする
-  // 以下のような string => int のような無茶なキャストの場合・・・？
-  int cell4 = doc.GetCell<int>("Message", "10");
-  printf("[%d]\n", cell4);
+    // 無効なrow headerを指定した場合
+    // double cell3 = doc.GetCell<double>("Float", "12345");
+    // => std::out_of_range
+    
+    // 期待と異なるデータ型を取得した場合 
+    // 可能な限り、キャストするらしいが、
+    // 以下のような string => int のような無茶なキャストの場合
+    // int cell4 = doc.GetCell<int>("Message", "10");
+    // => std::invalid_argument
+  } catch(std::out_of_range& e) {
+    printf("out_of_range catched.\n");
+    printf("%s\n", e.what());
+  } catch(std::invalid_argument& e) {
+    printf("invalid_argument catched.\n");
+    printf("%s\n", e.what());
+  } catch(std::exception& e) {
+    // 単なるthrowだとここに来る
+    printf("unexpected exception catched.\n");
+    printf("%s\n", e.what());
+  } catch(...) {
+    // ここは通らない
+    printf("unexpected exception catched. (2)\n");
+  }
 }
